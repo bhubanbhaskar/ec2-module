94cc2841f7033d7191e041d42e35b981986ba025
bhubanbhaskar <bhubanbhaskar91@gmail.com>
2025-06-04 09:22:09 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
+  ami = "ami-03a13a09a711d3871"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "my-first-ec2"
+  }
+}
